17e53f27b90db10e51a760f92fa77bf47c4f7542
oezeakachi <oezeakac@redhat.com>
2025-09-28 04:02:22 +0100

    ami-fix

--- modules/mysql-instance/main.tf
+++ modules/mysql-instance/main.tf
@@ -1,5 +1,98 @@
+# Create Security Group - SSH Traffic
+
+# Resource-1: Create VPC
+resource "aws_vpc" "vpc-dev" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    "Name" = "vpc-dev"
+  }
+}
+
+# Resource-2: Create Subnets
+resource "aws_subnet" "vpc-dev-public-subnet-1" {
+  vpc_id                  = aws_vpc.vpc-dev.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-west-1a"
+  map_public_ip_on_launch = true
+}
+
+# Resource-3: Internet Gateway
+resource "aws_internet_gateway" "vpc-dev-igw" {
+  vpc_id = aws_vpc.vpc-dev.id
+}
+
+# Resource-4: Create Route Table
+resource "aws_route_table" "vpc-dev-public-route-table" {
+  vpc_id = aws_vpc.vpc-dev.id
+}
+
+# Resource-5: Create Route in Route Table for Internet Access
+resource "aws_route" "vpc-dev-public-route" {
+  route_table_id         = aws_route_table.vpc-dev-public-route-table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.vpc-dev-igw.id
+}
+
+# Resource-6: Associate the Route Table with the Subnet
+resource "aws_route_table_association" "vpc-dev-public-route-table-associate" {
+  route_table_id = aws_route_table.vpc-dev-public-route-table.id
+  subnet_id      = aws_subnet.vpc-dev-public-subnet-1.id
+}
+
+resource "aws_security_group" "vpc-ssh" {
+  name        = "vpc-ssh"
+  description = "Dev VPC SSH"
+  vpc_id      = aws_vpc.vpc-dev.id
+  ingress {
+    description = "Allow Port 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Allow all ip and ports outboun"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create Security Group - Web Traffic
+resource "aws_security_group" "vpc-web" {
+  name        = "vpc-web"
+  vpc_id      = aws_vpc.vpc-dev.id
+  description = "Dev VPC web"
+  ingress {
+    description = "Allow Port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow Port 443"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all ip and ports outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
 resource "aws_instance" "mysql" {
   ami           = "ami-0ae53736fc234deff"
+  subnet_id              = aws_subnet.vpc-dev-public-subnet-1.id
   instance_type = var.instance_type
   tags = {
     Name = var.instance_name
